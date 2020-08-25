865371b35087997fa64b74659cda4674d8c5867d
Dominic Hawton <dchawton@gmail.com>
2020-08-25 15:23:36 +1000

    Minor style changes.

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -1,31 +1,26 @@
 <template>
-	<div class="w-full relative">
-		<div
-			class="text-center flex flex-col justify-center items-center rounded-sm absolute inset-0 text-white bg-black bg-opacity-50"
-		>
-			<div class="w-10/12 md:w-5/12 lg:w-5/12">
-				<h1
-					class="text-4xl md:text-5xl lg:text-5xl"
-					v-for="page in $static.gcms.homePages"
-					:key="page.id"
-				>
-					{{ page.heroTextLarge }}
-				</h1>
-				<p class="my-2 font-light text-md md:text-xl lg:text-xl ">
-					Free online mental-health courses and articles.
-				</p>
-				<g-link to="/courses">
-					<button class="btn-primary">See Courses</button>
-				</g-link>
-			</div>
-		</div>
-		<div class="w-full">
-			<g-image
-				src="~/assets/hero.jpg"
-				class="w-full bg-black rounded-sm p-0 m-0"
-			/>
-		</div>
-	</div>
+  <div class="w-full relative">
+    <div
+      class="text-center flex flex-col justify-center items-center rounded-sm absolute inset-0 text-white bg-black bg-opacity-50 image-container"
+    >
+      <div class="w-10/12 md:w-5/12 lg:w-5/12">
+        <h1
+          class="text-4xl md:text-5xl lg:text-5xl"
+          v-for="page in $static.gcms.homePages"
+          :key="page.id"
+        >{{ page.heroTextLarge }}</h1>
+        <p
+          class="my-2 font-light text-md md:text-xl lg:text-xl"
+        >Free online mental-health courses and articles.</p>
+        <g-link to="/courses">
+          <button class="btn-primary">See Courses</button>
+        </g-link>
+      </div>
+    </div>
+    <div class="w-full image-container">
+      <g-image src="~/assets/hero.jpg" class="w-full bg-black rounded-sm p-0 m-0" />
+    </div>
+  </div>
 </template>
 
 <static-query>
@@ -40,8 +35,12 @@ query {
 
 <script>
 export default {
-	name: 'Hero',
+  name: "Hero",
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+.image-container {
+  margin-top: -2rem;
+}
+</style>
--- src/templates/Course.vue
+++ src/templates/Course.vue
@@ -1,69 +1,64 @@
 <template>
-	<Layout>
-		<div>
-			<div>
-				<p
-					class="bg-gray-300 rounded-md text-gray-800 text-xs tracking-widest uppercase py-2 px-4 mb-4 inline-block mt-0 md:mt-0 lg:mt-10"
-				>
-					{{ $page.gcms.course.tag }}
-				</p>
-			</div>
-			<h1 class="my-3 text-3xl">{{ $page.gcms.course.title }}</h1>
-			<p class="mb-6">
-				Taught by <strong>{{ $page.gcms.course.instructor }}</strong>
-			</p>
-			<div class="flex justify-start items-center">
-				<Timer />
-				<p class="ml-2 text-left text-xs text-blue-500 py-2">
-					{{ $page.gcms.course.timeToComplete }} hours to complete.
-				</p>
-			</div>
-			<hr class="mt-4 mb-8" />
-			<g-image :src="$page.gcms.course.image.url" class="rounded-sm" />
-			<article class="mt-8">
-				<strong>
-					{{ $page.gcms.course.description }}
-				</strong>
-				<br />
-				<br />
-				Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus dolorem
-				voluptates deleniti blanditiis suscipit vitae tempore est? Maxime
-				nostrum officiis consectetur at. Nulla consectetur iste eius,
-				exercitationem, unde quasi autem expedita aliquam animi, libero
-				inventore modi reiciendis. Obcaecati, alias ipsam? Debitis voluptatibus
-				expedita exercitationem aperiam commodi necessitatibus ipsa rem
-				voluptate dolor modi laborum aut perferendis saepe, molestiae deserunt
-				in rerum minus dolores consectetur error veritatis accusantium
-				doloremque? Laudantium dignissimos, vero eveniet natus ullam libero
-				maiores iste eaque, excepturi laborum, sint exercitationem ad qui.
-				Fugit, dolorem at impedit harum in cum totam iusto sequi molestiae nisi
-				aliquam molestias suscipit nesciunt ipsam reprehenderit! Deleniti,
-				commodi.
-				<br />
-				<br />
-				Facilis fugit temporibus culpa totam incidunt saepe sunt, esse suscipit
-				odio enim, exercitationem harum tenetur illo accusantium aliquam veniam
-				itaque! Eum a modi necessitatibus rerum consectetur harum cumque maxime
-				nesciunt, dolor fugiat, voluptates ullam aperiam reprehenderit omnis
-				voluptatem. Doloribus nulla consequatur deleniti neque minus? Nostrum,
-				iste similique? Provident cum, impedit soluta distinctio minima debitis
-				quibusdam possimus a ipsam necessitatibus laboriosam, consequuntur et!
-				Officiis eaque possimus veritatis libero, incidunt vel corporis impedit
-				amet culpa reprehenderit consectetur, neque, tempora quos omnis tempore?
-				Eum quis itaque ut quidem quasi maiores blanditiis sequi, architecto
-				iste asperiores repudiandae. Quam natus molestias earum.
-			</article>
-		</div>
-	</Layout>
+  <Layout>
+    <div>
+      <div>
+        <p
+          class="bg-gray-300 rounded-md text-gray-800 text-xs tracking-widest uppercase py-2 px-4 mb-4 inline-block mt-0 md:mt-0 lg:mt-10"
+        >{{ $page.gcms.course.tag }}</p>
+      </div>
+      <h1 class="my-3 text-3xl">{{ $page.gcms.course.title }}</h1>
+      <p class="mb-6">
+        Taught by
+        <strong>{{ $page.gcms.course.instructor }}</strong>
+      </p>
+      <div class="flex justify-start items-center">
+        <Timer />
+        <p
+          class="ml-2 text-left text-xs text-blue-500 py-2"
+        >{{ $page.gcms.course.timeToComplete }} hours to complete.</p>
+      </div>
+      <hr class="mt-4 mb-8" />
+      <g-image :src="$page.gcms.course.image.url" class="rounded-sm" />
+      <article class="mt-8">
+        <strong>{{ $page.gcms.course.description }}</strong>
+        <br />
+        <br />Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus dolorem
+        voluptates deleniti blanditiis suscipit vitae tempore est? Maxime
+        nostrum officiis consectetur at. Nulla consectetur iste eius,
+        exercitationem, unde quasi autem expedita aliquam animi, libero
+        inventore modi reiciendis. Obcaecati, alias ipsam? Debitis voluptatibus
+        expedita exercitationem aperiam commodi necessitatibus ipsa rem
+        voluptate dolor modi laborum aut perferendis saepe, molestiae deserunt
+        in rerum minus dolores consectetur error veritatis accusantium
+        doloremque? Laudantium dignissimos, vero eveniet natus ullam libero
+        maiores iste eaque, excepturi laborum, sint exercitationem ad qui.
+        Fugit, dolorem at impedit harum in cum totam iusto sequi molestiae nisi
+        aliquam molestias suscipit nesciunt ipsam reprehenderit! Deleniti,
+        commodi.
+        <br />
+        <br />Facilis fugit temporibus culpa totam incidunt saepe sunt, esse suscipit
+        odio enim, exercitationem harum tenetur illo accusantium aliquam veniam
+        itaque! Eum a modi necessitatibus rerum consectetur harum cumque maxime
+        nesciunt, dolor fugiat, voluptates ullam aperiam reprehenderit omnis
+        voluptatem. Doloribus nulla consequatur deleniti neque minus? Nostrum,
+        iste similique? Provident cum, impedit soluta distinctio minima debitis
+        quibusdam possimus a ipsam necessitatibus laboriosam, consequuntur et!
+        Officiis eaque possimus veritatis libero, incidunt vel corporis impedit
+        amet culpa reprehenderit consectetur, neque, tempora quos omnis tempore?
+        Eum quis itaque ut quidem quasi maiores blanditiis sequi, architecto
+        iste asperiores repudiandae. Quam natus molestias earum.
+      </article>
+    </div>
+  </Layout>
 </template>
 
 <script>
-import Timer from '../assets/timer-outline.svg';
+import Timer from "../assets/timer-outline.svg";
 export default {
-	name: 'Course',
-	components: {
-		Timer,
-	},
+  name: "Course",
+  components: {
+    Timer,
+  },
 };
 </script>
 
